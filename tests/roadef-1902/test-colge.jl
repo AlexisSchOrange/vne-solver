e994fe4cdd052c7cc39c6a2e5a70f34a1769375f
AlexisSchOrange <alexis.schneider@orange.com>
2025-02-24 00:54:06 +0100

    OUIII

--- tests/roadef-1902/test-colge.jl
+++ tests/roadef-1902/test-colge.jl
@@ -28,7 +28,7 @@ function solve_all_instances_colge(path)
         push!(sns, g)
     end
 
-    overall_df = DataFrame(Gr=String[], Gs=String[], Algo=String[], Times=Int[], Res=Int32[], CGval=Float32[], LGBound=Float32[], nbcol=Int32[])
+    overall_df = DataFrame(Gr=String[], Gs=String[], Algo=String[], Times=Int[], NbSubG=Int32[], Res=Int32[], CGval=Float32[], LGBound=Float32[], nbcol=Int32[])
 
     for vn in vns
         println("Doing vn $(vn[][:name])...")
@@ -45,7 +45,7 @@ function solve_all_instances_colge(path)
             end
 
 
-            for time_limit in [20, 60]
+            for time_limit in [300, 1200, 6000]
                 res, nbsg, CG_bound, LG_bound, nb_col = solve_subgraph_decompo_tests(instance; time_max = time_limit, v_node_partitionning = [], nb_part = nb_subgraphs)
                 push!(overall_df, (instance.v_network[][:name], instance.s_network[][:name], "Colge", time_limit, nbsg, res, CG_bound, LG_bound, nb_col)) 
                 CSV.write("xp_colge_$(last(path, 4)[1:3]).csv", overall_df)
