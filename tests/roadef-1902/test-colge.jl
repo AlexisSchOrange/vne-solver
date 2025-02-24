36b7432186db2b456fb47595bf2b4151e388d89c
AlexisSchOrange <alexis.schneider@orange.com>
2025-02-25 00:16:02 +0100

    oupsi

--- tests/roadef-1902/test-colge.jl
+++ tests/roadef-1902/test-colge.jl
@@ -45,7 +45,7 @@ function solve_all_instances_colge(path)
             end
 
 
-            for time_limit in [6000]
+            for time_limit in [3600]
                 res, nbsg, CG_bound, LG_bound, nb_col = solve_subgraph_decompo_tests(instance; time_max = time_limit, v_node_partitionning = [], nb_part = nb_subgraphs)
                 push!(overall_df, (instance.v_network[][:name], instance.s_network[][:name], "Colge", time_limit, nbsg, res, CG_bound, LG_bound, nb_col)) 
                 CSV.write("xp_colge_$(last(path, 4)[1:3]).csv", overall_df)
